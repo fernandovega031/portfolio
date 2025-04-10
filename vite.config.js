f912ee5993e76516dd70d43865fda43cdcab5949
Fernando Vega <51734133+fernandovega031@users.noreply.github.com>
2025-04-09 18:31:42 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'http://www.justfernandosportfolio.site/',
+  base: 'justfernandosportfolio.site/',
   assetsInclude: ['**/*.JPG'],
   plugins: [react(),
   ],
